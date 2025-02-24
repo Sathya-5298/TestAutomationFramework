33b1fbc3600fae38f42c6b394997f299d62fb073
smurt <smurt@LAPTOP-88JOOBUG.lan>
2025-02-24 13:21:22 +0530

    changes

--- src/test/java/com/ui/tests/LoginTest.java
+++ src/test/java/com/ui/tests/LoginTest.java
@@ -15,22 +15,22 @@ public class LoginTest extends TestBase {
 	
 	Logger logger = LoggerUtility.getLogger(this.getClass());
 	
-//	@Test(description = "Verifies Login with Valid Credentails", groups = {"e2e","sanity"}, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestJsonDataProvider")
-//	public void loginTest(User user)
-//	{
-//		
-//		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(),user.getPassword()).getUserName(), "Suresh K");
-//
-//	}
-//	
-//	@Test(description = "Verifies Login with Valid Credentails", groups = {"e2e","sanity"}, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, 
-//			dataProvider = "LoginTestCSVDataProvider", retryAnalyzer = com.ui.listeners.MyRetryAnalyzer.class)
-//	public void loginCSVTest(User user)
-//	{
-//		
-//		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(),user.getPassword()).getUserName(), "Suresh K");
-//
-//	}
+	@Test(description = "Verifies Login with Valid Credentails", groups = {"e2e","sanity"}, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestJsonDataProvider")
+	public void loginTest(User user)
+	{
+		
+		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(),user.getPassword()).getUserName(), "Suresh K");
+
+	}
+	
+	@Test(description = "Verifies Login with Valid Credentails", groups = {"e2e","sanity"}, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, 
+			dataProvider = "LoginTestCSVDataProvider", retryAnalyzer = com.ui.listeners.MyRetryAnalyzer.class)
+	public void loginCSVTest(User user)
+	{
+		
+		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(),user.getPassword()).getUserName(), "Suresh K");
+
+	}
 	
 	@Test(description = "Verifies Login with Valid Credentails", groups = {"e2e","sanity"}, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestExcelDataProvider")
 	public void loginExcelTest(User user)
