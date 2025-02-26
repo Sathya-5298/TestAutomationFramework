aec4cf4acf26828b4d3935039ce7672061ee0bda
smurt <smurt@host.docker.internal>
2025-02-26 13:02:25 +0530

    Test

--- src/test/java/com/ui/tests/LoginTest.java
+++ src/test/java/com/ui/tests/LoginTest.java
@@ -23,19 +23,19 @@ public class LoginTest extends TestBase {
 
 	}
 	
-//	@Test(description = "Verifies Login with Valid Credentails", groups = {"e2e","sanity"}, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, 
-//			dataProvider = "LoginTestCSVDataProvider", retryAnalyzer = com.ui.listeners.MyRetryAnalyzer.class)
-//	public void loginCSVTest(User user)
-//	{
-//		
-//		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(),user.getPassword()).getUserName(), "Suresh K");
-//
-//	}
-//	
-//	@Test(description = "Verifies Login with Valid Credentails", groups = {"e2e","sanity"}, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestExcelDataProvider")
-//	public void loginExcelTest(User user)
-//	{
-//		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(),user.getPassword()).getUserName(), "Suresh K");
-//
-//	}
+	@Test(description = "Verifies Login with Valid Credentails", groups = {"e2e","sanity"}, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, 
+			dataProvider = "LoginTestCSVDataProvider", retryAnalyzer = com.ui.listeners.MyRetryAnalyzer.class)
+	public void loginCSVTest(User user)
+	{
+		
+		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(),user.getPassword()).getUserName(), "Suresh K");
+
+	}
+	
+	@Test(description = "Verifies Login with Valid Credentails", groups = {"e2e","sanity"}, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestExcelDataProvider")
+	public void loginExcelTest(User user)
+	{
+		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(),user.getPassword()).getUserName(), "Suresh K");
+
+	}
 }
--- src/test/java/com/ui/tests/TestBase.java
+++ src/test/java/com/ui/tests/TestBase.java
@@ -47,16 +47,16 @@ public class TestBase
 		return homePage;
 	}
 	
-//	@AfterMethod(description = "Tear Down the browser")
-//	public void tearDown()
-//	{
-//		if(isLambdaTest)
-//		{
-//			LambdaTestUtility.quitSession();
-//		}
-//		else
-//		{
-//			homePage.quit();
-//		}
-//	}
+	@AfterMethod(description = "Tear Down the browser")
+	public void tearDown()
+	{
+		if(isLambdaTest)
+		{
+			LambdaTestUtility.quitSession();
+		}
+		else
+		{
+			homePage.quit();
+		}
+	}
 }
